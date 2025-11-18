4cda8d31eeab44ca43163eba6dcbd501e3d2ff4c
amanuel tullu <amanueltu1@gmail.com>
2025-11-18 22:02:31 +0300

    Add files via upload

---
+++ main multiplication.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, multi;
+
+    cout << "Enter a: "<<endl;
+    cin >> a ;
+    cout << "Enter b : "<<endl;
+	cin>> b;
+
+    multi = a * b;
+
+    cout << "result= " << multi << endl;
+
+    return 0;
+}
+
